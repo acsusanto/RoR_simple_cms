46d8fa3f8859740e8bb5acd9893fb086091547c5
Alexander Susanto <susanto@live.com>
2020-08-17 17:34:13 +0700

    Update the page, section and subject models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,7 @@
 class Subject < ApplicationRecord
 
+    has_many :pages
+
     scope :visible, lambda { where(:visible => true) }
     scope :invinsible, lambda {where(:visble => false ) }
     scope :sorted, lambda { order("position ASC") }
