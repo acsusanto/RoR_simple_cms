4c0a9e4209b016406e9167ad485cb4e7f03b1bc6
Alexander Susanto <susanto@live.com>
2020-08-17 15:18:34 +0700

    Updated the subjects to have Scopes inside the classes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ApplicationRecord
+
+    scope :visible, lambda { where(:visible => true) }
+    scope :invinsible, lambda {where(:visble => false ) }
+    scope :sorted, lambda { order("position ASC") }
+    scope :newest_first, lambda { order("created_at DESC") }
+    scope :search, lambda {|query| where (["name LIKE ?", "%#{query}%"])}
+
 end
